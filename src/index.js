c457aaed43e5ead8ea6d7c0cc17b99b6cc64802d
Kollan House <kollanh@gmail.com>
2017-10-24 22:00:00 -0700

    added in coinmarketcap with basic html rendering.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require("path");
-const ticker = require('coinmarketcap-api');
+const CoinMarketCap = require('coinmarketcap-api');
 
 const app = express();
 
@@ -11,12 +11,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const client = new CoinMarketCap()
  
-client.getTicker().then(console.log).catch(console.error)
 client.getGlobal().then(console.log).catch(console.error)
 
 app.get('/', function (req, res) {
 	res.sendFile('index.html');
 });
+
+app.get('/market', function(req, res) {
+	client.getTicker({convert: 'USD'})
+	.then(result => {
+		res.status(201).send(result).end();
+	})
+	.catch(console.error)
+})
 
 app.listen(8080, function () {
   console.log('App Running');
