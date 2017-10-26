aa69ff44dd3bd81f38ac2b0d5d1b2c2b5cc7b31a
Kollan House <kollanh@gmail.com>
2017-10-25 17:34:47 -0700

    added in websocket with api call every 10 s or so. Added in html styling.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require("path");
+const http = require('http');
+const url = require('url');
 const CoinMarketCap = require('coinmarketcap-api');
+const WebSocket = require('ws');
 
 const app = express();
 
@@ -10,8 +13,29 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const client = new CoinMarketCap()
- 
-client.getGlobal().then(console.log).catch(console.error)
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws, req) {
+  const location = url.parse(req.url, true);
+  // You might use location.query.access_token to authenticate or share sessions
+  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  setInterval(function() {
+    console.log('tick')
+    client.getTicker({convert: 'USD'})
+    .then(result => {
+      ws.send(JSON.stringify(result))
+    })
+    .catch(console.error)
+  }, 10500);
+});
+
 
 app.get('/', function (req, res) {
 	res.sendFile('index.html');
@@ -24,6 +48,16 @@ app.get('/market', function(req, res) {
 	})
 	.catch(console.error)
 })
+
+app.use(function (req, res) {
+  res.send({ msg: "hello" });
+});
+
+
+
+server.listen(8088, function listening() {
+  console.log('Listening on %d', server.address().port);
+});
 
 app.listen(8080, function () {
   console.log('App Running');
